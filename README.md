fb06317a9a7dcb0b8cfd633f476e3be5b1278435
Cynthia Teeters <cynthiateeters@users.noreply.github.com>
2021-03-19 18:14:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dom Demo
+
+## Video by Brad Traversy
+
+[JavaScript Crash Course For Beginners - The Dom](https://youtu.be/hdI2bqOjy3c?t=4229) Start at 1:10:29
---
+++ main.js
@@ -0,0 +1,8 @@
+/* ATTENTION: THIS IS CODE FROM THE YOUTUBE CRASH COURSE. IT IS NOT MEANT TO RUN, IT IS JUST FOR LEARNING PURPOSES 
+
+Video by Brad Traversy
+JavaScript Crash Course For Beginners
+https://youtu.be/hdI2bqOjy3c?t=4228 
+
+Start at 1:10:29
+ */
